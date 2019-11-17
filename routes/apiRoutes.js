9f4f11b73a59f40402d7006adcfac1f3e7f45a45
codemouse <freedmouse@gmail.com>
2019-11-17 09:44:29 -0700

    latest apiRoutes file. Handles user and admin profile, create user and admin

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,8 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
     // POST route for saving a new user
-    app.post("/user/register", function(req, res) {
+    app.post("/user/register", function (req, res) {
         console.log(req.body);
         db.User.create({
             firstName: req.body.firstName,
@@ -13,13 +13,13 @@ module.exports = function(app) {
             phone: req.body.phone,
             startTime: req.body.startTime,
             endTime: req.body.endTime
-        }).then(function(dbUser) {
+        }).then(function (dbUser) {
             res.json(dbUser);
         });
     });
 
     // route for adding an administrator
-    app.post("/admin/register", function(req, res) {
+    app.post("/admin/register", function (req, res) {
         console.log(req.body);
         db.Organization.create({
             firstName: req.body.firstName,
@@ -29,32 +29,54 @@ module.exports = function(app) {
             org: req.body.org,
             volunteers: req.body.volunteers,
             hours: req.body.hours
-        }).then(function(dbOrganization) {
+        }).then(function (dbOrganization) {
             res.json(dbOrganization);
         });
     });
 
-    app.get("/api/users", function(req, res) {
-        db.User.findAll().then(function(dbUser) {
+    // gets all the database info and sends it to admin page
+    app.get("/admin/profile", function (req, res) {
+        db.Organization.findAll().then(function (dbOrganization) {
+            // console.log(dbOrganization[0]);
+            res.render("adminProfile", {
+                data: dbOrganization
+            });
+        });
+    });
+
+    // gets all the database info and sends it to user page
+    app.get("/user/profile", function (req, res) {
+        db.Organization.findAll().then(function (dbOrganization) {
+            // console.log(dbOrganization[0]);
+            res.render("userProfile", {
+                data: dbOrganization
+            });
+        });
+    });
+    // displays all users
+    app.get("/api/users", function (req, res) {
+        db.User.findAll().then(function (dbUser) {
             res.json(dbUser);
             console.log(dbUser);
         });
     });
 
-    app.get("/api/orgs", function(req, res) {
-        db.Organization.findAll().then(function(dbOrganization) {
+    // displays all the organizations
+    app.get("/api/orgs", function (req, res) {
+        db.Organization.findAll().then(function (dbOrganization) {
             res.json(dbOrganization);
         });
     });
-    app.post("/api/user/login", function(req, res) {
-        console.log(req.body);
-    });
-    app.get("/api/users/", function(_req, res) {
-        db.User.findAll({}).then(function(dbUser) {
+
+    // app.post("/api/user/login", function (req, res) {
+    //     console.log(req.body);
+    // });
+
+    // diplays all the users
+    app.get("/api/users/", function (_req, res) {
+        db.User.findAll({}).then(function (dbUser) {
             res.json(dbUser);
         });
     });
 };
 
-// capture data from login form and put in database
-// GET route for getting all of the users
